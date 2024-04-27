9d8bd6eaf03ed834c5f4e5ddbdd2f1de0fe2a460
alimuhammed_Dev <muhammedaliali2020@gmail.com>
2024-04-27 05:02:41 +0100

    final commit

--- src/App.jsx
+++ src/App.jsx
@@ -74,8 +74,9 @@ const App = () =>
         <div>
             <h1 className='border border-[#82776D] mt-10'></h1>
         </div>
-        <div className='text-[12px] mx-auto justify-center mt-10'>
-          <h1 className='flex items-center space-x-2'>2023 FASHION FIT <span><h1 className='bg-[#82776D] p-1 w-fit rounded-full ml-2'></h1></span> <p>All right reserved.</p> <span><p>Designed by Adenike Abioye</p></span></h1>
+        <div className='lg:flex lg:gap-5 text-[12px] items-center mx-auto justify-center mt-10'>
+          <h1 className='flex items-center space-x-2 justify-center text-center'>2023 FASHION FIT <span><h1 className='bg-[#82776D] p-1 w-fit rounded-full ml-2'></h1></span> <p>All right reserved.</p></h1>
+          <p className='text-center lg:text-left'>Designed by Adenike Abioye</p>
         </div>
       </div>
     </div>
