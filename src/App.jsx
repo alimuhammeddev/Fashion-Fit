4a68f992b8316cdf952f614e9e3f3a11fa6bbcf6
alimuhammed_Dev <muhammedaliali2020@gmail.com>
2024-05-09 12:49:22 +0100

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import styles from './style';
-import { Benefit, Blog, Collection, Footer, Hero, Navbar, Occassion, Reviews, Sellers, } from './Components';
 import About from './Pages/About';
 import Home from './Pages/Home';
 
